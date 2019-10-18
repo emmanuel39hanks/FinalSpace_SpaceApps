cd6ca377979be04ed900f145388570329f4bb286
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-10-18 11:22:58 -0700

    adding needed code for the globe

--- script.js
+++ script.js
@@ -47,3 +47,30 @@ let boundaries = [];
 boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
 boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
 boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
+
+let polygon = new WorldWind.Polygon(boundaries, polygonAttributes);
+polygon.extrude = true;
+polygonLayer.addRenderable(polygon);
+
+let modelLayer = new WorldWind.RenderableLayer();
+wwd.addLayer(modelLayer);
+
+let serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
+
+let layerName = "MOD_LSTD_CLIM_M";
+
+let createLayer = function (xmlDom) {
+    let wms = new WorldWind.WmsCapabilities(xmlDom);
+    let wmsLayerCapabilities = wms.getNamedLayer(layerName);
+    let wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+    let wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+    wwd.addLayer(wmsLayer);
+};
+
+let logError = function (jqXhr, text, exception) {
+    console.log("There was a failure retrieving the capabilities document: " +
+        text +
+    " exception: " + exception);
+};
+
+$.get(serviceAddress).done(createLayer).fail(logError);
