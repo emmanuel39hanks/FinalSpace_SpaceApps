30e825d98aedbb6d20e0c8d60753e8aa33f08d67
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-10-18 11:03:09 -0700

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,48 @@ let wwd = new WorldWind.WorldWindow("canvasOne");
 
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+wwd.addLayer(new WorldWind.CompassLayer());
+wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+let placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+wwd.addLayer(placemarkLayer);
+
+let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+placemarkAttributes.imageOffset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.3,
+    WorldWind.OFFSET_FRACTION, 0.0);
+
+placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 1.0);
+
+placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
+
+let position = new WorldWind.Position(55.0, -106.0, 100.0);
+let placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+placemark.label = "Placemark\n" +
+    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
+    "Lon " + placemark.position.longitude.toPrecision(5).toString();
+placemark.alwaysOnTop = true;
+
+placemarkLayer.addRenderable(placemark);
+
+let polygonLayer = new WorldWind.RenderableLayer();
+wwd.addLayer(polygonLayer);
+
+let polygonAttributes = new WorldWind.ShapeAttributes(null);
+polygonAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.75);
+polygonAttributes.outlineColor = WorldWind.Color.BLUE;
+polygonAttributes.drawOutline = true;
+polygonAttributes.applyLighting = true;
+
+
+let boundaries = [];
+boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
+boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
+boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
